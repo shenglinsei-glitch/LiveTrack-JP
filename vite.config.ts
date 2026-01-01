68d7007720a503e62ee0add578d14f8ddf1983f1
LIN <shenglin.sei@gmail.com>
2026-01-01 18:04:03 +0900

    clean: 删除多余的 dist 文件夹，统一使用 docs

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,15 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: './', // 确保使用相对路径
       server: {
         port: 3000,
         host: '0.0.0.0',
+      },
+      // 👇 关键：强制让 Vite 把结果吐到 docs 文件夹
+      build: {
+        outDir: 'docs',
+        emptyOutDir: true, // 打包前清空旧的 docs
       },
       plugins: [react()],
       define: {
